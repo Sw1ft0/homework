42bc53a75a8d73081268e45c2341e953a719b15a
Sw1ft0 <pasha.k460@gmail.com>
2022-09-22 07:12:03 +0100

    fix

--- Lab1/lab1_1.py
+++ Lab1/lab1_1.py
@@ -8,7 +8,7 @@ op = {'+': lambda x, y: x + y,
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('first', type=float)
-    parser.add_argument('operator')
+    parser.add_argument('operator', type=str, choices = ["+","-","*","/"])
     parser.add_argument('second', type=float)
 
     args = parser.parse_args()
